d63cb39133ec6f83e41df9897d4280bbb18947ec
ft-yjh <1943778865@qq.com>
2020-12-16 11:20:48 +0800

    多态技术的小项目练习

--- 多态/职工管理.cpp
+++ 多态/职工管理.cpp
@@ -1,125 +1,172 @@
 #include<iostream>
 using namespace std;
 
+int startNum = 1;
+
 class Employee
 {
 	public:
-		Employee()
-		{
-			
-		}
-		virtual ~Employee() = 0;
-		virtual void promote(int increment = 0) = 0;
-		virtual void getpay() = 0;
-		virtual void disinfo() = 0;
-	
-	protected:
-		string _name;
-		int _num;
-		int _grade;
-		float _salary;
-};
-
-class Manager:virtual public Employee
-{
-	Manager()
-	{
+		virtual void init() = 0;
+		virtual void promote() = 0;
+		virtual void getPay() = 0;
+		virtual void disInfo() = 0;
 		
-	}
-	virtual ~Manager() = 0;
-	virtual void promote(int crement = 0)
-	{
+		protected:
+			string _name;
+			int _num;
+			int _grade;
+			int _salary;
+			static int startNum;
 		
-	}
-	virtual void getpay()
-	{
-		
-	}
-	virtual void disinfo()
-	{
-		
-	}
-	
-	protected:
-		float _fixsalary;
 };
 
 class Technian:public Employee
 {
-	public: 
+	public:
 	Technian()
 	{
-		
-	}
-	virtual ~Technian()
-	{
-		
-	}
-	virtual void promote(int incredent = 0)
-	{
-	}
-	virtual void getpay()
-	{
-	}
-	virtual void disinfo()
-	{
+		_moneyperhour = 100;
 	}
 	protected:
-		int moneyperhour;
-		int hourcount;
+		
+	virtual void init()
+	{
+		cout << "请输入技术人员的姓名: " << endl;
+		cin >> _name;
+		cout << "请输入技术人员的编号" <<endl;
+		cin >>_num; 
+		cout << "请输入本月工作的时长" <<endl;
+		cin >> _houramount; 
+	}
+		
+	virtual void promote()
+	{
+		_grade = 3;
+	}
+	virtual void getPay()
+	{
+		_salary = _houramount * _moneyperhour;
+	}
+	virtual void disInfo()
+	{
+		cout << "编号: " << _num << " 级别:" << _grade
+		<<" 姓名: " << _name << " 工资: " << _salary<<endl; 
+	}
+	
+	int _moneyperhour;
+	int _houramount;
 	
 };
 
-class Salesman
+class Manager:virtual public Employee
 {
-	Salesman(){
-	}
-	virtual ~Salesman(){
-	}
-	virtual void promote(int incredent = 0)
-	{
-	}
-	virtual void getpay()
-	{
-	}
-	virtual void disinfo()
-	{
-	}
+	public:
 	protected:
-		float saleamount;
-		float persent;
+		
+	virtual void init()
+	{
+		cout << "请输入经理的姓名: " << endl;
+		cin >> _name;
+		cout << "请输入经理的编号" <<endl;
+		cin >>_num;
+	}
+		
+	virtual void promote()
+	{
+		_grade = 4; 
+	}
+	virtual void getPay()
+	{
+		_salary = 8000; 
+	}
+	virtual void disInfo()
+	{
+		cout << "编号: " << _num << " 级别:" << _grade 
+		<<" 姓名: " << _name << " 工资: " << _salary<<endl; 
+	} 
 };
 
-class SaleManager:public Salesman,public Manager
+class Salesman:virtual public Employee
 {
-	SalesManager(){
-	}
-	virtual ~SalesManager(){
-    }
-    virtual void promote(int incredent = 0)
+	public:
+	Salesman()
 	{
-	}
-	virtual void getpay()
+		_rate = 0.04;
+	} 
+	protected:	
+	virtual void init()
 	{
+		cout << "请输入推销员的姓名: " << endl;
+		cin >> _name;
+		cout << "请输入推销员的编号" <<endl;
+		cin >>_num; 
+		cout << "请输入本月的销售额" <<endl;
+		cin >> _monthsale; 
 	}
-	virtual void disinfo()
+		
+	virtual void promote()
 	{
+	    _grade = 1;
 	}
+	virtual void getPay()
+	{
+		_salary = _rate * _monthsale ;
+	}
+	virtual void disInfo()
+	{
+		cout << "编号: " << _num << " 级别: " << _grade  
+		<<" 姓名: " << _name << " 工资: " << _salary<<endl; 
+	}
+	int _monthsale;
+	float _rate;
+};
+
+class SalesManager:public Manager,public Salesman
+{
+	public:
+		SalesManager()
+		{
+			_fixsalary = 5000;
+			_rate = 0.05;
+		}
+	virtual void init()
+	{
+		cout << "请输入销售经理的姓名: " << endl;
+		cin >> _name;
+		cout << "请输入销售经理的编号" <<endl;
+		cin >>_num;
+		cout << "请输入本月的销售额"<<endl;
+		cin >> _monthsale; 
+	}
+		
+	virtual void promote()
+	{
+		_grade = 4; 
+	}
+	virtual void getPay()
+	{
+		_salary = _fixsalary + _monthsale * _rate; 
+	}
+	virtual void disInfo()
+	{
+		cout << "编号: " << _num << " 级别:" << _grade 
+		<<" 姓名: " << _name << " 工资: " << _salary<<endl; 
+	} 
 	
+	int _fixsalary;
 };
 
 int main()
 {
+	Employee *emp[] = {new Technian,new Manager,new Salesman,new SalesManager};
 	int i = 0;
-	Employee *p[] = {new Manager,new Technian,new Salesman,SaleManager};
-	int len = sizeof(p)/sizeof(p[0]);
+	int len = sizeof(emp)/sizeof(emp[0]);
 	for (i = 0; i < len; i++)
 	{
-		p[i]->promote();
-		p[i]->getpay();
-		p[i]->disinfo();
+		emp[i]->init();
+	    emp[i]->promote();
+	    emp[i]->getPay();
+	    emp[i]->disInfo();
+	    cout << "+++++++++++++++++"<<endl;
 	}
-	
-	
-	return 0;
  } 
